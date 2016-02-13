97455f6a03c5c079a9175d7a4923d723ee86ab07
Anthony Simpson <ansi6622@colorado.edu>
2016-02-13 00:50:30 -0700

    deleted entirety of nodejs it was a learning experience but i was wasting time so i copied the full code from elliotbonneville, and then corrected two small syntax errors

--- Node.js
+++ Node.js
@@ -1,60 +1,98 @@
-request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-    return;
-  }
+var request = require("request"),
+	cheerio = require("cheerio"),
+	url = "https://www.google.com/search?q=data+mining",
 
-  // load the body of the page into Cheerio so we can traverse the DOM
-  var $ = cheerio.load(body),
-    links = $(".r a");
+	corpus = {},
+	totalResults = 0,
+	resultsDownloaded = 0;
 
-  links.each(function (i, link) {
-    // get the href attribute of each link
-    var url = $(link).attr("href");
+function callback () {
+	resultsDownloaded++;
 
-    // strip out unnecessary junk
-    url = url.replace("/url?q=", "").split("&")[0];
+	if (resultsDownloaded !== totalResults) {
+		return;
+	}
 
-    if (url.charAt(0) === "/") {
-      return;
-    }
+	var words = [];
 
-    // this link counts as a result, so increment results
-    totalResults++;
+	// stick all words in an array
+	for (prop in corpus) {
+		words.push({
+			word: prop,
+			count: corpus[prop]
+		});
+	}
 
-    text = text.replace(/\s+/g, " ")
-       .replace(/[^a-zA-Z ]/g, "")
-       .toLowerCase();
+	// sort array based on how often they occur
+	words.sort(function (a, b) {
+		return b.count - a.count;
+	});
 
-
-// Split on spaces for a list of all the words on that page and
-// loop through that list.
-text.split(" ").forEach(function (word) {
-  // We don't want to include very short or long words because they're
-  // probably bad data.
-  if (word.length > 20) {
-    return;
-  }
-
-  if (corpus[word]) {
-    // If this word is already in our corpus, our collection
-    // of terms, increase the count for appearances of that
-    // word by one.
-    corpus[word]++;
-  } else {
-    // Otherwise, say that we've found one of that word so far.
-    corpus[word] = 1;
-  }
-});
-
-for (prop in corpus) {
-  words.push({
-    word: prop,
-    count: corpus[prop]
-  });
+	// finally, log the first fifty most popular words
+	console.log(words.slice(0, 20));
 }
 
-// sort array based on how often they occur
-words.sort(function (a, b) {
-  return b.count - a.count;
+request(url, function (error, response, body) {
+	if (error) {
+		console.log("Couldn’t get page because of error: " + error);
+		return;
+	}
+
+	// load the body of the page into Cheerio so we can traverse the DOM
+	var $ = cheerio.load(body),
+		links = $(".r a");
+
+	links.each(function (i, link) {
+		// get the href attribute of each link
+		var url = $(link).attr("href");
+
+		// strip out unnecessary junk
+		url = url.replace("/url?q=", "").split("&")[0];
+
+		if (url.charAt(0) === "/") {
+			return;
+		}
+
+		// this link counts as a result, so increment results
+		totalResults++;
+
+		// download that page
+		request(url, function (error, response, body) {
+			if (error) {
+				console.log("Couldn’t get page because of error: "  + error);
+				return;
+			}
+
+			// load the page into cheerio
+			var $page = cheerio.load(body),
+				text = $page("body").text();
+
+			// throw away extra whitespace and non-alphanumeric characters
+			text = text.replace(/\s+/g, " ")
+					   .replace(/[^a-zA-Z ]/g, "")
+					   .toLowerCase();
+
+			// split on spaces for a list of all the words on that page and
+			// loop through that list
+			text.split(" ").forEach(function (word) {
+				// we don't want to include very short or long words, as they're
+				// probably bad data
+				if (word.length < 4 || word.length > 20) {
+					return;
+				}
+
+				if (corpus[word]) {
+					// if this word is already in our "corpus", our collection
+					// of terms, increase the count by one
+					corpus[word]++;
+				} else {
+					// otherwise, say that we've found one of that word so far
+					corpus[word] = 1;
+				}
+			});
+
+			// and when our request is completed, call the callback to wrap up!
+			callback();
+		});
+	});
 });
